c1fea788f14cfe4fa56573b091b4df6cd8ffab3a
milanjuneja <milan.juneja97@gmail.com>
2025-01-16 14:17:53 +0530

    Added spring security and JWT implementation

---
+++ ecommerce/src/main/java/com/ecommerce/config/JWT_CONSTANT.java
@@ -0,0 +1,6 @@
+package com.ecommerce.config;
+
+public class JWT_CONSTANT {
+    public static final String SECRET_KEY="agioaugoiadygyadgyad89gya89gyyagaygaygyahghaghuaghu56465yhhe5g5y58d9aygdyg8dayg8oayga8yg8";
+    public static final String JWT_HEADER="Authorization";
+}
