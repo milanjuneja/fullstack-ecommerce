d6d88ec6f943244d3d0f0245da317be41f668835
milanjuneja <milan.juneja97@gmail.com>
2025-01-21 22:54:01 +0530

    adding wishlist

--- ecommerce/src/main/java/com/ecommerce/modal/WishList.java
+++ ecommerce/src/main/java/com/ecommerce/modal/WishList.java
@@ -7,11 +7,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode
 public class WishList {
 
     @Id
@@ -24,4 +19,36 @@ public class WishList {
     @ManyToMany
     private Set<Product> products = new HashSet<>();
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
+    public WishList() {
+    }
+
+    public WishList(Long id, User user, Set<Product> products) {
+        this.id = id;
+        this.user = user;
+        this.products = products;
+    }
 }
