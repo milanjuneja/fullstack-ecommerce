e4e86f43fb86e7cae9919a61a6ac6ab6e7035815
milanjuneja <milan.juneja97@gmail.com>
2025-01-24 12:19:34 +0530

    add filter functionality

---
+++ ecommerce-frontend/src/data/filter/color.ts
@@ -0,0 +1,50 @@
+export const colors = [
+  {
+    "name": "Black",
+    "hex": "#000000"
+  },
+  {
+    "name": "Purple",
+    "hex": "#800080"
+  },
+  {
+    "name": "Red",
+    "hex": "#FF0000"
+  },
+  {
+    "name": "Blue",
+    "hex": "#0000FF"
+  },
+  {
+    "name": "Green",
+    "hex": "#008000"
+  },
+  {
+    "name": "White",
+    "hex": "#FFFFFF"
+  },
+  {
+    "name": "Yellow",
+    "hex": "#FFFF00"
+  },
+  {
+    "name": "Cyan",
+    "hex": "#00FFFF"
+  },
+  {
+    "name": "Magenta",
+    "hex": "#FF00FF"
+  },
+  {
+    "name": "Gray",
+    "hex": "#808080"
+  },
+  {
+    "name": "Orange",
+    "hex": "#FFA500"
+  },
+  {
+    "name": "Pink",
+    "hex": "#FFC0CB"
+  }
+];
