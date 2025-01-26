06a179e564bbbc796cede1296d6eb778d276f11f
milanjuneja <milan.juneja97@gmail.com>
2025-01-26 12:40:25 +0530

    add user details and address components to account page

--- ecommerce-frontend/src/customer/pages/Account/Account.tsx
+++ ecommerce-frontend/src/customer/pages/Account/Account.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import Orders from './Orders'
 import OrderDetails from './OrderDetails'
+import UserDetails from './UserDetails'
+import Address from './Address'
 
 const menu = [
   {name: "orders", path: "/account/orders"},
@@ -43,7 +45,9 @@ const Account = () => {
 
         <section className='right lg:col-span-2 lg:pl-5 py-5'>
           {/* <Orders /> */}
-          <OrderDetails />
+          {/* <OrderDetails /> */}
+          {/* <UserDetails /> */}
+          <Address />
         </section>
       </div>
     </div>
---
+++ ecommerce-frontend/src/customer/pages/Account/UserAddressCard.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const UserAddressCard = () => {
+  return (
+    <div className="p-5 border rounded-md flex">
+      <div className="space-y-3">
+        <h1>User 1</h1>
+        <p className="w-[320px]">gagag gagdg, aggag ag ag , agg , 201100</p>
+        <p>
+          <strong>Mobile :</strong> 53535235
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default UserAddressCard;
