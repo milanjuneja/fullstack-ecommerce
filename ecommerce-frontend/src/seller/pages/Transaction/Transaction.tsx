bf4e0b87f71ca1f7f117b0c8ca65d38fa106ae51
milanjuneja <milan.juneja97@gmail.com>
2025-01-27 12:02:56 +0530

    add seller dashboard pages and components

---
+++ ecommerce-frontend/src/seller/pages/Transaction/Transaction.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TransactionTable from './TransactionTable'
+
+const Transaction = () => {
+  return (
+    <div>
+      <TransactionTable />
+    </div>
+  )
+}
+
+export default Transaction
